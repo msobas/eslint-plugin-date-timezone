32ad8dcdbb9a1f89ac6de3693ec0d7dafaf04423
msobas <mateusz.sobas@gmail.com>
2021-08-29 12:24:26 +0200

    Fix tests

--- tests/lib/rules/no-date-from-string.js
+++ tests/lib/rules/no-date-from-string.js
@@ -27,29 +27,29 @@ var expectedErrors = [
     message: "Please don't use Date with string values",
     type: "NewExpression",
   },
-],
+];
 
 var ruleTester = new RuleTester();
 ruleTester.run("eslint-plugin-date-timezone", rule, {
   valid: [
     // give me some code that won't trigger a warning
     "new Date()",
-    "Date(1577833200000)",
+    "new Date(1577833200000)",
     "new Date(2020, 0, 1)",
     "Date('2020-01-01')", // does not check CallExpression
   ],
 
   invalid: [
     {
-      code: 'new Date("2020-01-01")',
+      code: "new Date('2020-01-01')",
       errors: expectedErrors,
     },
     {
-      code: 'new Date("2020-01-01T00:00:00")',
+      code: "new Date('2020-01-01T00:00:00')",
       errors: expectedErrors,
     },
     {
-      code: 'const localisedDate = new Date("2020-01-01");',
+      code: "const localisedDate = new Date('2020-01-01');",
       errors: expectedErrors,
     },
   ],
